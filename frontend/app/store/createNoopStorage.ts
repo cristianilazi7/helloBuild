d80569354755f0f023addeab982d4a8a89cbbe51
cristianilazi7 <cristiancastroa86@gmail.com>
2025-03-07 21:41:06 -0500

    fix: type

--- frontend/app/store/createNoopStorage.ts
+++ frontend/app/store/createNoopStorage.ts
@@ -3,13 +3,13 @@ import createWebStorage from "redux-persist/lib/storage/createWebStorage";
 
 const createNoopStorage = () => {
     return {
-        getItem(_key: any) {
+        getItem() {
             return Promise.resolve(null);
         },
-        setItem(_key: any, value: any) {
+        setItem(_key: string, value: string) {
             return Promise.resolve(value);
         },
-        removeItem(_key: any) {
+        removeItem() {
             return Promise.resolve();
         },
     };
